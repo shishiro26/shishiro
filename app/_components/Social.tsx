34b268422d238c255202f1784101309b750df7a8
shishiro <dheeravathshishiro@gmail.com>
2024-02-05 15:01:37 +0530

    social

--- app/_components/Social.tsx
+++ app/_components/Social.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const Social = () => {
   return (
-    <div>Social</div>
-  )
-}
+    <div>
+      <Image
+        src={"/svgs/github.svg"}
+        alt="github"
+        width={24}
+        height={24}
+        className="m-1"
+      />
+    </div>
+  );
+};
 
-export default Social
+export default Social;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Intro from "./_components/Intro";
+import Social from "./_components/Social";
 
 export default function Home() {
   return (
     <section className="lg:ml-32 2xl:ml-52 2xl:mt-10">
       <Intro />
+      <Social />
     </section>
   );
 }
